d777ba93668b37293dcda5b29d5d4f485d3148dd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2021-10-13 22:03:40 +0530

    Create declarations.h

---
+++ declarations.h
@@ -0,0 +1,11 @@
+#include "funct.h" //header file containing the foreward declarations for functions in funct.h header file
+
+int random_number();
+
+int user_input();
+
+int check();
+
+int score();
+
+int begin_game();
