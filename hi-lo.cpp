4033d817fe9f773cb19da465f6ddd4d224cd7c70
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2021-10-14 21:48:42 +0530

    Update hi-lo.cpp

--- hi-lo.cpp
+++ hi-lo.cpp
@@ -6,12 +6,15 @@ int main()
               << "Instructions for the game\n"
               << "1)Number will range from 1-10\n"
               << "2)You will get 4 outputs{\"bingo\",\"Hi\",\"Lo\",\"Far Away\"}\n";
-    int no_of_turns{0}; // No of times the user has played the game
-    int no_of_rounds{0}
-    int total_score{};  // Score of the user
-    int opt{1};         // default value of opt variable for in-game_opts
-    bool mark = false;  
-    begin_game();
+    int no_of_turns{0};  // No of times the user has played the game
+    int no_of_rounds{0}; // No of rounds user played on one go
+    int total_score{};   // Score of the user
+    int opt{1};          // default value of opt variable for in-game_opts
+    bool mark = false;
+    if (begin_game() == 1)
+    {
+        no_of_rounds++;
+    }        
     while (mark != true)
     {
         no_of_turns++;
